67ef06b951f2adc261e20454ec099cd86b9650b5
jeunseon <euns420@naver.com>
2023-03-14 14:33:30 +0900

    js 결과페이지 공유하기

--- js/share.js
+++ js/share.js
@@ -1,3 +1,35 @@
+const url = 'https://mbti-inflearn.netlify.app/';
+function setShare(){
+    let resultImg = document.querySelector('#resultImg');
+    let resultAlt = resultImg.firstElementChild.alt;
+    const shareTitle = '십이간지 연애유형 결과';
+    const shareDes = infoList[resultAlt].name;
+    const shareImage = url + 'img/image-' + resultAlt + '.png';
+    const shareURL = url + 'page.result' + resultAlt + '.html';
+    
+    Kakao.Share.sendDefault({
+        objectType: 'feed',
+        content: {
+            title: shareTitle,
+            description: shareDes,
+            imageUrl: shareImage,
+            link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL,
+            },
+        },
+        buttons: [
+            {
+                title: '결과 확인하기',
+                link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL,
+                },
+            },
+        ],
+    });
+}
+
 function kakaoShare(){   
     Kakao.Share.sendDefault({
         objectType: 'feed',
